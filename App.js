4b048f3c8c573001e64e58c154b7542c380c01a8
Salman Arshad <salman.arshad@codingcops.com>
2023-01-26 17:44:34 +0500

    comment added

--- App.js
+++ App.js
@@ -1,24 +1,67 @@
 import logo from './logo.svg';
 import './App.css';
 
+// function formatName(user) {
+//   return user.firstName + ' ' + user.lastName;
+// }
+
+// const user = {
+//   firstName: 'Salman',
+//   lastName: 'Arshad'
+// };
+// function getGreeting(user) {
+//   if (user) {
+//     return <h1>Hello, {formatName(user)}!</h1>;
+//   }
+//   return <h1>Hello, Stranger.</h1>;
+// }
+// const element = (
+//   <h1>
+//     Hello, {getGreeting(user)}!
+//   </h1>
+// );
+// const element = <a href="https://www.reactjs.org"> link </a>;
+// const element = <img src="https://images.pexels.com/photos/674010/pexels-photo-674010.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"></img>;
+// const title = response.potentiallyMaliciousInput;
+// // This is safe:
+// const element = <h1>{title}</h1>;
+// const element = react.createElement(
+//   'h1',
+//   {className: 'greeting'},
+//   'Hello, world!'
+// );
+const element = {
+  type: 'h1',
+  props: {
+    className: 'greeting',
+    children: 'Hello, world!'
+  }
+};
+function concat(element)
+{
+  return  '<'+element.type+'>'+element.props.className+' '+element.props.children+'<'+"/"+element.type+'>';
+}
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // fragmentation
+    <>
+      <div>
+      concat(element)
+        <p> </p>
+      </div>
+      <div>
+        <h1>hello world</h1>
+        <p> iusdhfn</p>
+      </div>
+    </>
+    // <>sakdjaslkdjjasdkj</>
+    // aliabbas
+
+
+
+
+
   );
 }
 
